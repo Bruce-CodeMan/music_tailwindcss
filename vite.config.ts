335f80d1e048ec0b8c8af4013a38462b35586496
Bruce Hsu <1033684650@qq.com>
2023-07-17 10:40:47 +0800

    using @ replace the src folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+/*
+ * @Date: 2023-07-17 10:36:30
+ * @Author: Bruce Hsu
+ * @Description: 
+ */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{
+      find: "@",
+      replacement: path.resolve(__dirname, "src")
+    }]
+  }
 })
