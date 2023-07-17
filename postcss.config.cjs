5df23036e20ef9f4dd9fdd315a12f31a9441497e
Bruce Hsu <1033684650@qq.com>
2023-07-17 10:44:17 +0800

    config the tailwindcss

---
+++ postcss.config.cjs
@@ -0,0 +1,11 @@
+/*
+ * @Date: 2023-07-17 10:41:40
+ * @Author: Bruce Hsu
+ * @Description: 
+ */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/*
+ * @Date: 2023-07-17 10:41:40
+ * @Author: Bruce Hsu
+ * @Description: 
+ */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
